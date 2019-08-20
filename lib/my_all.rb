094c052453295397f1924384300365b7b5094226
Daniel Reina <dannyflatiron@gmail.com>
2019-08-20 02:28:35 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  new_array = []
+  while collection.length > counter
+    new_array << yield(collection[counter])
+    counter += 1
+  end
+  if new_array.include?(false)
+    false
+  else
+    true
+  end
 end
